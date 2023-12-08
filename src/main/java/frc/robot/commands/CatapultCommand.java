771d09d99c6ad5b17580457db41007dcb9153ec9
Yishay Aizenman <yishayzen@gmail.com>
2023-12-08 11:39:45 +0200

    commit

--- src/main/java/frc/robot/commands/CatapultCommand.java
+++ src/main/java/frc/robot/commands/CatapultCommand.java
@@ -5,26 +5,28 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.CatapultSubsystem;
 
-public class ChargeCatapult extends CommandBase {
-  /** Creates a new ChargeCatapult. */
-  public ChargeCatapult() {
-    // Use addRequirements() here to declare subsystem dependencies.
+public class CatapultCommand extends CommandBase {
+  private double speed;
+  private CatapultSubsystem catapultSubsystem;
+  public CatapultCommand(CatapultSubsystem catapultSubsystem,double speed) {
+    this.speed=speed;
+    this.catapultSubsystem=catapultSubsystem;
+    addRequirements(catapultSubsystem);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    catapultSubsystem.setCatapultMotor(speed);
+  }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
--- src/main/java/frc/robot/subsystems/CatapultSubsystem.java
+++ src/main/java/frc/robot/subsystems/CatapultSubsystem.java
@@ -5,13 +5,20 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class CatapultSubsystem extends SubsystemBase {
-  /** Creates a new CatapultSubsystem. */
-  public CatapultSubsystem() {}
+  private PWMSparkMax catapultMotor = new PWMSparkMax(0);
+  public CatapultSubsystem() {
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void setCatapultMotor(double speed){
+    catapultMotor.set(speed);
   }
 }
